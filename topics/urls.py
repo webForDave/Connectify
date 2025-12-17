6b7a9d5e72be8a698c0fbfc63fea48aec89e0a84
David Akinola <dakinola54@gmail.com>
2025-12-17 08:52:46 +0100

    Project configuration and scaffolding

--- topics/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path("topics/<slug:slug>/create/", views.create_new_topic_view, name="create_topic"),
-    path("topics/<slug:slug>/", views.topic_detail_view, name="topic_detail"),
-    path("topics/<slug:slug>/update/", views.update_topic, name="update_topic"),
-    path("topics/<slug:slug>/delete/", views.delete_topic_view, name="delete_topic"),
-]
