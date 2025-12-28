727ce3128819869c714d8e427fd06407967e903e
David Akinola <dakinola54@gmail.com>
2025-12-28 16:11:37 +0100

    modified new user permission

--- communities/views.py
+++ communities/views.py
@@ -10,6 +10,8 @@ class UserIsNotNew(BasePermission):
     message = 'You must be a member for at least 30 days to create a community.'
 
     def has_permission(self, request, view):
+            if request.method == 'GET':
+                return True
             return request.user.is_authenticated and not request.user.user_joined_recently()
             
     
