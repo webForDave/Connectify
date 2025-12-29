4da92a7080414f13ca035182a279ea413dd17dad
David Akinola <dakinola54@gmail.com>
2025-12-29 23:40:24 +0100

    Ordering on communities table

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from rest_framework import status
 from rest_framework.response import Response
-from .models import Post
+from .models import Post, Comment
 from communities.models import Community
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
@@ -87,8 +87,41 @@ def comment_view_create(request, community_name, post_title):
 
         if serializer.is_valid():
             serializer.save(post_id=post.id, comment_author=request.user)
-            # post.post_comments.add()
-            # post.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticatedOrReadOnly])
+def comment_details(request, community_name, post_title, comment_slug):
+
+    try:
+        community = Community.objects.get(community_name__iexact=community_name)
+    except Community.DoesNotExist:
+        return Response({'communities': 'Community not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+    try:
+        post = Post.objects.get(title__iexact=post_title)
+    except Post.DoesNotExist:
+        return Response({'posts': 'Post not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+    try:
+        comment = Comment.objects.get(comment_slug=comment_slug)
+    except Comment.DoesNotExist:
+        return Response({'comments': 'Comment not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = PostCommentsSerializer(comment)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        if request.user != comment.comment_author:
+            return Response({'comments': 'You do not have permission to perform this action'}, status=status.HTTP_403_FORBIDDEN)
+        serializer = CreateCommentSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
