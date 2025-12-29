66add0a3620386fc8150e701c17ca311223f485f
David Akinola <dakinola54@gmail.com>
2025-12-29 23:18:50 +0100

    Additional field on user details serializer

--- accounts/views.py
+++ accounts/views.py
@@ -13,7 +13,7 @@ User = get_user_model()
 def user_details(request, username):
 
     try:
-        user = User.objects.get(username=username)
+        user = User.objects.get(username__iexact=username)
     except User.DoesNotExist:
         return Response({'users': 'user not found'}, status=status.HTTP_404_NOT_FOUND)
 
@@ -26,12 +26,16 @@ def user_details(request, username):
             serializer = CustomRegularUserDetailsSerializer(user)
             return Response(serializer.data)
         
+    if request.user != user:
+        return Response({'users': 'You do not have permission to perform this action'}, status=status.HTTP_403_FORBIDDEN)
+        
     if request.method == 'PUT':
+
         serializer = CustomUpdateUserSerializer(user, data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return Response({'users': 'user updated succeefully'}, status=status.HTTP_200_OK)
+            return Response({'users': 'user updated sucessfully'}, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
