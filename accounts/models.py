0495dd768b6d9bc11a46d9297d2e874a492682f5
David Akinola <dakinola54@gmail.com>
2026-01-01 09:12:10 +0100

    App design: moved custom user manager to separate file

--- accounts/models.py
+++ accounts/models.py
@@ -1,39 +1,13 @@
-
 # core Django
-from django.db import models
 from django.contrib.auth.models import(
     AbstractBaseUser, 
-    BaseUserManager, 
     PermissionsMixin,
 )
-from django.db.models.signals import post_save
+from django.db import models
 from django.utils import timezone
-from django.dispatch import receiver
 
-
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password, **extra_fields):
-        if not email:
-            raise ValueError('An email is a requirement')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save()
-        return user
-    
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_active', True)
-
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Super user must have is_superuser=True')
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Super user must have is_staff=True')
-        if extra_fields.get('is_active') is not True:
-            raise ValueError('Super user must have is_active=True')
-        
-        return self.create_user(email, password, **extra_fields)
+# accounts app
+from .managers import CustomUserManager
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=25, null=True, blank=True)
@@ -53,6 +27,4 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     
     def user_joined_recently(self):
         return (timezone.now() - self.date_joined).days < 30
-
-
-        
+    
